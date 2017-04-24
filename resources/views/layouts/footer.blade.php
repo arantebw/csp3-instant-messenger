d80ccea698a56cf7341593589dd1cbf58a3f50d0
Billy Wilson Arante <arantebillywilson@gmail.com>
2017-04-24 16:52:57 +0800

    Created logout landing page

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,14 +1,10 @@
-<div class="container-fluid main-footer text-muted">
-    <div class="container">
-        <div class="row">
-            <div class="col-sm-6">
-                <i class="fa fa-registered" aria-hidden="true"></i>
-                Messenger for developers
-            </div>
-            <div class="col-sm-6 text-right">
-                <p><i class="fa fa-code" aria-hidden="true"></i> with <i class="fa fa-heart" aria-hidden="true"></i> by <a href="#">Billy Wilson Arante</a></p>
-                <p>Powered by <a href="#">Tuitt Coding Bootcamp</a></p>
-            </div>
+<footer class="container text-muted padding-10px">
+    <div class="row">
+        <div class="text-center col-sm-12 col-md-6" style="margin: 0 auto;">
+            <small>Created by <a href="#">Billy Wilson Arante</a></small>
+        </div>
+        <div class="text-center col-sm-12 col-md-6" style="margin: 0 auto;">
+            <small>Powerd by <a href="#">Tuitt Coding Bootcamp</a></small>
         </div>
     </div>
-</div>
+</footer>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 @extends ('layouts.master_alternative')
 
 @section ('title')
-    reoslack &middot; Thank you for using Reoslack!
+    reoslack &middot; Welcome to Reoslack!
 @endsection
 
 @section ('header')
@@ -17,20 +17,20 @@
         @include ('layouts.info')
 
         <div class="card">
-            <h3 class="card-header text-center">Thank you for using Reoslack!</h3>
+            <h3 class="card-header text-center">Welcome to Reoslack!</h3>
 
             <div class="card-block">
                 <div class="text-center">
-                    <h1 class="card-text text-muted"><strong><em>Reoslack, the messenger of awesome web developers.</em></strong></h1>
+                    <h1 class="card-text text-muted"><strong><em>Reoslack, the replication of slack.</em></strong></h1>
                 </div>
             </div>
 
             <div class="card-block">
                 <div class="text-center">
-                    <a href="/signin" class="btn btn-success btn-sm">Sign in</a>
-                    <a href="/members/create" class="btn btn-outline-primary btn-sm">Sign up</a>
-                    <a href="/teams/create" class="btn btn-outline-primary btn-sm">Create a team</a>
-                    <a href="/join" class="btn btn-outline-primary btn-sm">Join a team</a>
+                    <a href="/signin" class="btn btn-success btn-lg">Sign in</a>
+                    <a href="/members/create" class="btn btn-outline-primary btn-lg">Sign up</a>
+                    <a href="/teams/create" class="btn btn-outline-primary btn-lg">Create a team</a>
+                    <a href="/join" class="btn btn-outline-primary btn-lg">Join a team</a>
                 </div>
             </div>
         </div>
