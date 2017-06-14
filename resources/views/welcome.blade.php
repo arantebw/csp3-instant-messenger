e2b95b88ae608edcd7bf60870e2c4e5d0c96eefc
Billy Wilson Arante <arantebillywilson@gmail.com>
2017-06-15 01:04:21 +0800

    Updated RWD of login buttons

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -26,19 +26,19 @@
 
             <div class="card-block">
                 <div class="text-center">
-                    <a href="/signin" class="btn btn-success btn-lg">
+                    <a href="/signin" class="btn btn-success btn-lg login-btn">
                         <i class="fa fa-sign-in" aria-hidden="true"></i>
                         Sign in
                     </a>
-                    <a href="/members/create" class="btn btn-outline-primary btn-lg">
+                    <a href="/members/create" class="btn btn-outline-primary btn-lg login-btn">
                         <i class="fa fa-hand-o-right" aria-hidden="true"></i>
                         Sign up
                     </a>
-                    <a href="/teams/create" class="btn btn-outline-primary btn-lg">
+                    <a href="/teams/create" class="btn btn-outline-primary btn-lg login-btn">
                         <i class="fa fa-users" aria-hidden="true"></i>
                         Create a team
                     </a>
-                    <a href="/join" class="btn btn-outline-primary btn-lg">
+                    <a href="/join" class="btn btn-outline-primary btn-lg login-btn">
                         <i class="fa fa-user-plus" aria-hidden="true"></i>
                         Join a team
                     </a>
